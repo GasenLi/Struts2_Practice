51d8cb8981d39df5fa9b1f856514849970042cea
GasenLi <250316711@qq.com>
2018-11-29 21:28:59 +0800

    :sheep: test4 stuManage BG complete

---
+++ src/Action/StudentManage.java
@@ -0,0 +1,152 @@
+package Action;
+
+import Dao.BaseDao;
+import Entity.Student;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import java.util.List;
+
+public class StudentManage extends ActionSupport implements ModelDriven<Student> {
+    private BaseDao baseDao = new BaseDao();
+    private JSONObject jsonObj;
+
+    private Student student = new Student();
+    private String option;
+
+    private String menu;
+    private String range;
+    private String value;
+    private String classID;
+
+    private JSONArray result = new JSONArray();
+
+    public JSONArray getResult() {
+        return result;
+    }
+
+    public void setResult(JSONArray result) {
+        this.result = result;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getClassID() {
+        return classID;
+    }
+
+    public void setClassID(String classID) {
+        this.classID = classID;
+    }
+
+    @Override
+    public Student getModel() {
+        return student;
+    }
+
+    @Override
+    public String execute() throws Exception {
+
+        //System.out.println("ID  " + student.getStudentID() + " ====");
+        String optionResult = null;
+
+        switch (option){
+            case "insert": optionResult = baseDao.insertObj(student);break;
+            case "delete": optionResult = baseDao.delete(student, student.getStudentID());break;
+            case "search": List<Object> searchResults = search();
+                           convertListToJson(searchResults);break;
+            case "update": optionResult = baseDao.updateObj(student);break;
+        }
+
+        if(!option.equals("search")){
+            jsonObj = new JSONObject();
+            jsonObj.put("optionResult", optionResult);
+
+            result.add(jsonObj);
+        }
+
+        return SUCCESS;
+    }
+
+    public void convertListToJson(List<Object> lists){
+        for(int i=0; i<lists.size(); i++){
+            jsonObj = new JSONObject();
+            student = (Student) lists.get(i);
+
+            jsonObj.put("studentID", student.getStudentID());
+            jsonObj.put("studentName", student.getStudentName());
+            jsonObj.put("studentAge", student.getStudentAge());
+            jsonObj.put("classID", student.getClassID());
+
+            result.add(jsonObj);
+        }
+    }
+
+    public void convertMenu(){
+        switch (menu){
+            case "name": menu = "StudentName";break;
+            case "age": menu = "StudentAge";break;
+            case "ID": menu = "StudentID";break;
+        }
+    }
+
+    public void convertRange(){
+            switch (range){
+                case "greater": range = ">";break;
+                case "gore": range = ">=";break;
+                case "equal": range = "=";break;
+                case "sore": range = "<=";break;
+                case "smaller": range = "<";break;
+
+                case "include": range = "like";break;
+            }
+    }
+
+    public List<Object> search(){
+        convertMenu();
+        convertRange();
+
+        String hql = "from Student ";
+        hql += "where ClassID = " + student.getClassID();
+
+        if(!range.equals("like")){
+            hql += " and " + menu + " " + range + " " + value;
+        }else {
+            hql += " and " + menu + " " + range + " '%" + value + "%'";
+        }
+
+        return baseDao.custom(hql);
+    }
+}
--- src/Dao/BaseDao.java
+++ src/Dao/BaseDao.java
@@ -5,43 +5,59 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class BaseDao {
+    private static Configuration configuration = new Configuration().configure();
     private SessionFactory sessionFactory;
     private Session session;
     private Transaction transaction;
 
     public BaseDao(){
-        Configuration configuration = new Configuration().configure();
         sessionFactory = configuration.buildSessionFactory();
         session = sessionFactory.openSession();
         transaction = session.beginTransaction();
     }
 
-    public void updateObj(Object o){
+    public String updateObj(Object o){
         session.update(o);
 
         over();
+        return "true";
     }
 
-    public void insertObj(Object o){
+    public String insertObj(Object o){
         session.save(o);
 
         over();
+        return "true";
     }
 
-    public Object searchObj(Object o, String id) throws IllegalAccessException, InstantiationException {
+    public Object searchObjByID(Object o, String id){
         o = session.get(o.getClass(), id);
 
         over();
         return o;
     }
 
-    public void delete(Object o, String id){
+
+    public String delete(Object o, String id){
         o = session.get(o.getClass(), id);
         session.delete(o);
 
         over();
+        return "true";
+    }
+
+    public List<Object> custom(String hql){
+        Query query = session.createQuery(hql);
+
+        List<Object> objectList = query.list();
+
+        over();
+        return  objectList;
     }
 
     private void over(){
@@ -52,15 +68,21 @@ public class BaseDao {
     }
 
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-        Student student = new Student();
-
-        student.setStudentID("0010");
-        student.setClassID("116030804");
-        student.setStudentName("小黑");
-        student.setStudentAge(18);
-
+//        Student student = new Student();
+//
+//        student.setStudentID("0010");
+//        student.setClassID("116030804");
+//        student.setStudentName("小黑");
+//        student.setStudentAge(18);
+//
         BaseDao baseDao = new BaseDao();
-        baseDao.delete(student, "0010");
+//        baseDao.delete(student, "0010");
+
+        String hql = "from Student where ClassID = 116030804 and StudentName like '%李%'  ";
+        List<Object> objectList = baseDao.custom(hql);
+        for(Object student : objectList){
+            System.out.println(student);
+        }
     }
 
 }
